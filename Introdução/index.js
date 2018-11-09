c06601c0cba559c70b3b925cbde93128069dcafe
Leandro Lopes <leandro.lopes.dev@gmail.com>
2018-11-09 14:29:16 -0200

    introducao ao javaScript

---
+++ Introdução/index.js
@@ -0,0 +1,11 @@
+console.log('Hello Word');
+
+let a = 10;
+const b = "10";
+
+
+console.log(a === b); // sinal de igualdade
+
+console.log(a !== b); // sinal de negação
+
+console.log(a != b && a == b)
